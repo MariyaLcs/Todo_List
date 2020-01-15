51e00f370c2070f897584d6e23417a5955609e85
Mariya Lucas <mariya.podosinova@gmail.com>
2020-01-15 15:17:26 +0000

    styling

--- assets/js/effects.js
+++ assets/js/effects.js
@@ -19,6 +19,8 @@ $("input[type='text']").keypress(function(event) {
     var todoText = $(this).val();
     $(this).val("");
     //create a new li and add to ul
-    $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    $("ul").append(
+      "<li><span><i class='fas fa-trash-alt'></i> " + todoText + "</li>"
+    );
   }
 });
