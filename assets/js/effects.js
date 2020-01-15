e09191209004e2c58170af81584999bbf4ca63d4
Mariya Lucas <mariya.podosinova@gmail.com>
2020-01-15 10:15:52 +0000

    marking completion with logic

---
+++ assets/js/effects.js
@@ -0,0 +1,14 @@
+//Check Off Specific Todos By Clicking
+$("li").click(function() {
+  if ($(this).css("color") === "rgb(128, 128, 128)") {
+    $(this).css({
+      color: "black",
+      textDecoration: "none"
+    });
+  } else {
+    $(this).css({
+      color: "grey",
+      textDecoration: "line-through"
+    });
+  }
+});
